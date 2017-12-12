704dbf516814b66928c4dde178d09f55b21ccc95
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-12-12 14:18:42 +0100

    Introduce Experimental and UseExperimental annotations

---
+++ compiler/testData/diagnostics/testsWithStdLib/experimental/useExperimentalOnFile.kt
@@ -0,0 +1,36 @@
+// FILE: api.kt
+
+package api
+
+@Experimental(Experimental.Level.WARNING, [Experimental.Impact.COMPILATION])
+@Target(AnnotationTarget.FUNCTION)
+annotation class CompilationExperimentalAPI
+
+@Experimental(Experimental.Level.WARNING, [Experimental.Impact.RUNTIME])
+@Target(AnnotationTarget.FUNCTION)
+annotation class RuntimeExperimentalAPI
+
+@CompilationExperimentalAPI
+fun compilation() {}
+
+@RuntimeExperimentalAPI
+fun runtime() {}
+
+// FILE: usage.kt
+
+@file:UseExperimental(CompilationExperimentalAPI::class)
+package usage
+
+import api.*
+
+fun use() {
+    compilation()
+    <!EXPERIMENTAL_API_USAGE!>runtime<!>()
+}
+
+class Use {
+    fun use() {
+        compilation()
+        <!EXPERIMENTAL_API_USAGE!>runtime<!>()
+    }
+}
